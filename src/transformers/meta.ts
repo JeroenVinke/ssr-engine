0b4cc0a6c85ab966763e715fba3bccc47b60e19c
Jeroen Vinke <jeroenvinke17@gmail.com>
2018-07-07 19:42:40 +0200

    feat(transformer): added meta tags transformer

---
+++ src/transformers/meta.ts
@@ -0,0 +1,15 @@
+import {appendToHead} from './utils';
+import {RenderOptions, TransformerContext} from '../interfaces';
+
+export default function(html: string, transformerCtx: TransformerContext, options: RenderOptions) {
+  const metaTags = transformerCtx.document.head.querySelector('meta');
+  if (metaTags) {
+    const metaTagsArray = Array.prototype.slice.call(metaTags);
+
+    for (let i = 0; i < metaTagsArray.length; i++) {
+      html = appendToHead(html, metaTagsArray[i].outerHTML);
+    }
+  }
+
+  return html;
+};
